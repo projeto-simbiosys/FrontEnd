d6cdd6536b16bd9d56ebe409e14f2da5768493d6
Matheus Castro <matheus.csilva@sptech.school>
2025-03-12 08:48:59 -0300

    feat: add input validation

--- pages/register/index.js
+++ pages/register/index.js
@@ -8,12 +8,32 @@ buttonSubmit.addEventListener('click', event => {
     handleRegister();
 });
 
-function handleRegister() {
-    const nameIsValid = validateName(iptName);
-    const emailIsValid = validateEmail(iptEmail);
-    const passwordIsValid = validatePassword(iptPassword.value);
+const inputErrorMessages = {
+    name: {
+        isEmpty: 'Nome é obrigatório'
+    },
+    email: {
+        isEmpty: 'Email é obrigatório',
+        emailFormatIsInvalid: 'Formato inválido'
+    },
+    password: {
+        isEmpty: 'Senha é obrigatória',
+        passwordLengthIsinvalid: 'Deve ter no mínimo 8 caracteres',
+        passwordHasNoSpecialCharacter: 'Deve ter no mínimo 1 caractereespecial'
+    }
+}
 
-    if(nameIsValid && emailIsValid && passwordIsValid){
+function handleRegister() {
+    const { isNameValid, errorName } = validateName(iptName);
+    const { isEmailValid, errorEmail } = validateEmail(iptEmail);
+    const { isPasswordValid, errorPassword } = validatePassword(iptPassword);
+
+    showErrorMessage(iptName, errorName);
+    showErrorMessage(iptEmail, errorEmail);
+    showErrorMessage(iptPassword, errorPassword);
+
+    console.log(isNameValid, isEmailValid, isPasswordValid);
+    if(isNameValid && isEmailValid && isPasswordValid){
         const newUser = {
             name: iptName.value,
             email: iptEmail.value,
@@ -26,26 +46,52 @@ function handleRegister() {
 
 function validateName(iptName) {
     const isEmpty = isEmptyInput(iptName.value);
-    return isEmpty;
+    const errorName = isEmpty ? inputErrorMessages.name.isEmpty : "";
+
+    return { isNameValid: !isEmpty, errorName };
 }
 
 function validateEmail(iptEmail) {
     const emailFormatted = formatInputEmail(iptEmail.value);
 
-    const isEmpty = isEmptyInput(emailFormatted);
-    const emailFormatIsValid = validateEmailFormat(emailFormatted);
+    const isEmailEmpty = isEmptyInput(emailFormatted);
+    const isEmailFormatValid = validateEmailFormat(emailFormatted);
+
+    let errorEmail = "";
+
+    if(!isEmailFormatValid) {
+        errorEmail = inputErrorMessages.email.emailFormatIsInvalid;
+    }
+
+    if(isEmailEmpty) {
+        errorEmail = inputErrorMessages.email.isEmpty;
+    }
     
-    return { isEmpty, emailFormatIsValid };
+    return { isEmailValid: !isEmailEmpty && isEmailFormatValid, errorEmail };
 }
 
 function validatePassword(iptPassword) {
     const passwordFormatted = formatInputPassword(iptPassword.value);
 
-    const isEmpty = isEmptyInput(passwordFormatted);
+    const isPasswordEmpty = isEmptyInput(passwordFormatted);
     const passwordLengthIsValid = hasValidLength(passwordFormatted);
     const passwordHasSpecialCharacter = hasSpecialCharacter(passwordFormatted);
 
-    return { isEmpty, passwordLengthIsValid, passwordHasSpecialCharacter };
+    let errorPassword = "";
+
+    if(!passwordHasSpecialCharacter) {
+        errorPassword = inputErrorMessages.password.passwordHasNoSpecialCharacter;
+    }
+    
+    if(!passwordLengthIsValid) {    
+        errorPassword = inputErrorMessages.password.passwordLengthIsinvalid;
+    }
+
+    if(isPasswordEmpty) {
+        errorPassword = inputErrorMessages.password.isEmpty;
+    }
+
+    return { isPasswordValid: !isPasswordEmpty && passwordLengthIsValid && passwordHasSpecialCharacter, errorPassword };
 }
 
 function showErrorMessage(element, message) {
