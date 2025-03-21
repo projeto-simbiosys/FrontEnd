3fc600f5267c429bb5561f2ff0269c97c3e059a7
Matheus Castro <matheus.csilva@sptech.school>
2025-03-21 17:45:47 -0300

    feat: componente MenuHamburguer

---
+++ src/icons/Close.jsx
@@ -0,0 +1,21 @@
+const CloseIcon = props => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <g id="Menu / Close_MD">
+      <path
+        id="Vector"
+        stroke="#9933FF"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="m18 18-6-6m0 0L6 6m6 6 6-6m-6 6-6 6"
+      ></path>
+    </g>
+  </svg>
+);
+
+export default CloseIcon;
