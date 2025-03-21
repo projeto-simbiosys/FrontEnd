5294059adcc96f724441c4d03579183b68cca5b0
Matheus Castro <matheus.csilva@sptech.school>
2025-03-21 15:06:05 -0300

    feat: button component

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,19 @@
+export default function Button({ variant, children, ...props }) {
+  const variantMapping = {
+    "inst-primary": "bg-main text-white",
+    "inst-secondary":
+      "bg-white text-secondary border border-solid border-secondary",
+    "inst-light": "bg-white text-main",
+    "inst-link":
+      "bg-transparent text-main !px-[0px] !py-[0px] shadow-none underline",
+  };
+
+  return (
+    <button
+      className={`${variantMapping[variant]} flex gap-[10px] items-center px-[20px] py-[10px] text-base font-semibold rounded-[5px] shadow-button cursor-pointer hover:brightness-95`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
