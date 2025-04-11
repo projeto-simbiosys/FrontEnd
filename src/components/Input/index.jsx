0f8886ee8a144c59b6e289e22892c80d9bcb0dbc
Matheus Castro <matheus.csilva@sptech.school>
2025-04-11 16:58:33 -0300

    fix: input component

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,15 +1,44 @@
-export default function Input({ children, hasError = false, ...props }) {
+import EyeClosed from "../../icons/EyeClosed";
+import EyeOpen from "../../icons/EyeOpen";
+import useInput from "./hook";
+
+export default function Input({ hasError = false, ...props }) {
+  const { input } = useInput(props.type);
+
   return (
     <div
-      className={`w-full flex items-center border ${
+      className={`group w-full flex items-center border ${
         hasError ? "border-red-700" : "border-sys-main"
       } has-[:disabled]:bg-gray-disabled has-[:disabled]:border-gray-disabled rounded-md px-1.5 py-1.5 gap-2 bg-white shadow-sm`}
     >
       <input
         {...props}
-        className="disabled:text-gray-detail-disabled disabled:cursor-not-allowed flex bg-transparent text-base text-sys-main placeholder-sys-main/40"
+        type={input.type}
+        className={`${
+          hasError ? "text-red-700" : "text-sys-main"
+        } disabled:text-gray-detail-disabled disabled:cursor-not-allowed flex bg-transparent text-base placeholder-sys-main/40`}
       />
-      {children}
+      {input.isPassword && (
+        <button
+          disabled={props.disabled}
+          onClick={input.tooglePassword}
+          className="group"
+        >
+          {input.showPassword ? (
+            <EyeOpen
+              className={`w-[25px] h-[25px] ${
+                hasError ? "fill-red-700" : "fill-sys-main"
+              } group-has-[:disabled]:fill-gray-detail-disabled`}
+            />
+          ) : (
+            <EyeClosed
+              className={`w-[25px] h-[25px] ${
+                hasError ? "fill-red-700" : "fill-sys-main"
+              } group-has-[:disabled]:fill-gray-detail-disabled`}
+            />
+          )}
+        </button>
+      )}
     </div>
   );
 }
