194944d4c59502e365aa673c4a30d979a1466e43
Matheus Castro <matheus.csilva@sptech.school>
2025-04-12 17:24:21 -0300

    feat: select disabled and error

--- src/components/Select/index.jsx
+++ src/components/Select/index.jsx
@@ -1,15 +1,25 @@
 import Dropdown from "../../icons/Dropdown";
 
-export default function Select({ children, ...props }) {
+export default function Select({ hasError = false, children, ...props }) {
   return (
-    <div className="grid border border-sys-main rounded-md px-1.5 py-1.5 gap-2 bg-white shadow-sm">
+    <div
+      className={`grid border ${
+        hasError ? "border-red-700" : "border-sys-main"
+      } has-[:disabled]:border-gray-detail-disabled has-[:disabled]:bg-gray-disabled rounded-md px-1.5 py-1.5 gap-2 bg-white shadow-sm`}
+    >
       <select
-        className="peer col-start-1 row-start-1 appearance-none text-sys-main"
+        className={`peer col-start-1 row-start-1 appearance-none ${
+          hasError ? "text-red-700" : "text-sys-main"
+        } disabled:text-gray-detail-disabled`}
         {...props}
       >
         {children}
       </select>
-      <Dropdown className="peer-focus:rotate-180 peer-focus-within:rotate-0 peer-disabled:fill-gray-detail-disabled w-[25px] h-[25px] fill-sys-main pointer-events-none col-start-1 row-start-1 ml-auto" />
+      <Dropdown
+        className={`peer-focus:rotate-180 peer-focus-within:rotate-0 peer-disabled:fill-gray-detail-disabled w-[25px] h-[25px] ${
+          hasError ? "fill-red-700" : "fill-sys-main"
+        } pointer-events-none col-start-1 row-start-1 ml-auto`}
+      />
     </div>
   );
 }
